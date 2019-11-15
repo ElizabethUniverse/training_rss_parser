856bd381a6048f348527eea5004bcbaa159049da
Elizaveta Lapunova <liza.lapunova99@gmail.com>
2019-11-15 15:57:18 +0300

    iteration 3 without tests

--- ClassNews.py
+++ ClassNews.py
@@ -32,7 +32,7 @@ def xml_arguments_for_class(xml_string, limit):
                 list_links = []
                 for group1 in re.finditer(LINKS_TEMPLATE, xml_news_item.text):
                     list_links.append(group1.group(1))
-                    parser_dictionary['links'] = list_links
+                parser_dictionary['links'] = list_links
 
         dict_article_list.append(parser_dictionary)
 
@@ -47,21 +47,39 @@ def dicts_to_articles(dict_list):
         article_list.append(MyArticle(item))
     return article_list
 
+
 @dataclass
 class MyArticle:
     """This is news class, which receives dictionary and have title, date, link, article and links keys fields"""
     def __init__(self, article_dict):
-        self.title = article_dict['title']
+        link_temp = "\'((http|https)://(\w|.)+?)\'"
         self.date = article_dict['date']
+        self.title = article_dict['title']
         self.link = article_dict['link']
         self.article = article_dict['article']
-        self.links_list = article_dict['links']
+
+        if type(article_dict['links']) == list:
+            self.links = article_dict['links']
+
+        if type(article_dict['links']) == str:
+            #print(article_dict['links'])
+            list_links = []
+            for group1 in re.finditer(link_temp, article_dict['links']):
+                list_links.append(group1.group(1))
+            self.links = list_links
+
 
     def __str__(self):
         result_string_article = '\n'
         result_string_article += "Title: {}\nDate: {}\nLink: {}\n\n{}\n\n".format(self.title, self.date, self.link,
                                                                                   self.article)
-        for link_idx, link in enumerate(self.links_list):
+        for link_idx, link in enumerate(self.links):
             result_string_article += "[{}]: {}\n".format(link_idx + 1, link)
         result_string_article += '\n'
         return result_string_article
+
+    def __eq__(self, other):
+        if self.article == other.article and self.title == other.title and self.link == other.link and \
+                self.date == other.date:
+            return True
+        return False
